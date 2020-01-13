fbd90cdef691c15c883bc72649eb5cb9b77992a5
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-13 23:55:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,10 +6,4 @@ class Student < InteractiveRecord
   attr_accessor :name, :grade
   attr_reader :id
 
-  def initialize(id: nil, name:, grade:)
-    @name = name
-    @grade = grade
-    @id = id
-  end
-
 end
